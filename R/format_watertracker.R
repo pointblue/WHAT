4fe0827cd894abe1fd2e657fe8d72740dc5c75d5
Kristen Dybala <kdybala@gmail.com>
2023-08-17 12:56:20 -0700

    add .data bindings

--- R/format_watertracker.R
+++ R/format_watertracker.R
@@ -8,6 +8,7 @@
 #'
 #' @return tibble with added fields year, month, wateryear, month_name, status, midpoint
 #' @export
+#' @importFrom rlang .data
 #'
 #' @examples
 #' # format_watertracker(df)
@@ -16,19 +17,22 @@ format_watertracker = function(df, wetthreshold = 50, obsthreshold = 80) {
   df |>
     # find mosaic midpoint
     dplyr::rowwise() |>
-    dplyr::mutate(midpoint = mean(c(MosaicDateStart, MosaicDateEnd))) |>
+    dplyr::mutate(midpoint = mean(c(.data$MosaicDateStart,
+                                    .data$MosaicDateEnd))) |>
     dplyr::tibble() |>
     # get year and month; translate to water year
-   dplyr:: mutate(year = format(midpoint, '%Y') |>  as.numeric(),
-                  month = format(midpoint, '%m') |>  as.numeric(),
-                  wateryear = dplyr::if_else(month >= 10, year + 1, year),
-                  month_name = format(midpoint, '%b') |>
+   dplyr:: mutate(year = format(.data$midpoint, '%Y') |>  as.numeric(),
+                  month = format(.data$midpoint, '%m') |>  as.numeric(),
+                  wateryear = dplyr::if_else(.data$month >= 10,
+                                             .data$year + 1, .data$year),
+                  month_name = format(.data$midpoint, '%b') |>
                     factor(levels = c('Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar',
                                       'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'))
                   ) |>
     # interpret flooding status as management mode on each scene date
     dplyr::mutate(
-      status = dplyr::if_else(PercentWater > wetthreshold, 'wet', 'dry'),
-      status = dplyr::if_else(PercentObserved < obsthreshold, NA, status)) |>
-    dplyr::select(year:status, dplyr::everything())
+      status = dplyr::if_else(.data$PercentWater > wetthreshold, 'wet', 'dry'),
+      status = dplyr::if_else(.data$PercentObserved < obsthreshold,
+                              NA, .data$status)) |>
+    dplyr::select(.data$year:.data$status, dplyr::everything())
 }
