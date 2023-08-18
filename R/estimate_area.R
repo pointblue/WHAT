f892cebefc8a02814d597ca860302eae7e992ed2
Kristen Dybala <kdybala@gmail.com>
2023-08-17 17:56:51 -0700

    remove support for groupby; add ungroup

--- R/estimate_area.R
+++ R/estimate_area.R
@@ -6,11 +6,9 @@
 #' @details This function converts the "ObservedArea" field (m^2) in the
 #'   original Water Tracker output data to an estimate of the total area of each
 #'   management unit, in acres. The area is estimated as the maximum of all
-#'   ObservedArea values with the same grouping ID, which by default is "unit".
+#'   ObservedArea values with the unit name.
 #'
 #' @param df tibble resulting from [format_watertracker()]
-#' @param groupby Character string for the field by which area should be
-#'   estimated
 #'
 #' @return tibble with additional field "area_ac"
 #' @export
@@ -18,8 +16,9 @@
 #'
 #' @examples
 #' #estimate_area(df)
-estimate_area = function(df, groupby = 'unit') {
+estimate_area = function(df) {
   df |>
-    dplyr::group_by(!!groupby) |>
-    dplyr::mutate(area_ac = max(.data$ObservedArea) / 900 / 10000 * 2.47105)
+    dplyr::group_by(.data$unit) |>
+    dplyr::mutate(area_ac = max(.data$ObservedArea) / 900 / 10000 * 2.47105) |>
+    dplyr::ungroup()
 }
