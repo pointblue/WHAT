d684f4b68fe993f408652a6dccf75664b2b9d16e
Kristen Dybala <kdybala@gmail.com>
2023-08-17 17:09:54 -0700

    add support for groupby

--- R/estimate_area.R
+++ R/estimate_area.R
@@ -1,6 +1,16 @@
 #' Estimate area
 #'
+#' Estimate the area of each wetland management unit from the area observed in
+#' the Water Tracker data.
+#'
+#' @details This function converts the "ObservedArea" field (m^2) in the
+#'   original Water Tracker output data to an estimate of the total area of each
+#'   management unit, in acres. The area is estimated as the maximum of all
+#'   ObservedArea values with the same grouping ID, which by default is "unit".
+#'
 #' @param df tibble resulting from [format_watertracker()]
+#' @param groupby Character string for the field by which area should be
+#'   estimated
 #'
 #' @return tibble with additional field "area_ac"
 #' @export
@@ -8,9 +18,8 @@
 #'
 #' @examples
 #' #estimate_area(df)
-estimate_area = function(df) {
+estimate_area = function(df, groupby = 'unit') {
   df |>
-    dplyr::mutate(area_ac = max(.data$ObservedArea) / 900 / 10000 * 2.47105) |>
-    dplyr::select(.data$ID, .data$unit, .data$area_ac, dplyr::everything())
-
+    dplyr::group_by(!!groupby) |>
+    dplyr::mutate(area_ac = max(.data$ObservedArea) / 900 / 10000 * 2.47105)
 }
