c94328fe85227827f7141c9314ef1ddee28b233d
Kristen Dybala <kdybala@gmail.com>
2023-08-22 17:23:36 -0700

    add sampledat and documentation

---
+++ R/sampledat.R
@@ -0,0 +1,17 @@
+#' Sample Water Tracker data for demonstrating package functions
+#' @format
+#' A tibble with 369 rows and 10 columns
+#' \describe{
+#'    \item{Name}{Name of sample wetland management unit}
+#'    \item{Mosaic}{Unique ID for the mosaic of satellite images included}
+#'    \item{MosaicDateStart}{Earliest date of satellite imagery mosaic}
+#'    \item{MosaicDateEnd}{Latest date of satelliate imagery mosaic}
+#'    \item{ObservedArea}{Total area (m^2) for which flooding status could be interpreted}
+#'    \item{ObservedAreaWater}{Total area (m^2) estimated to be flooded}
+#'    \item{PercentObserved}{Percent of total area for which flooding status could be interpreted}
+#'    \item{PercentWater}{Percent of observed area estimated to be flooded}
+#'    \item{EstimatedAreaWAter}{?}
+#'    \item{Threshold}{?}
+#' }
+#'
+"sampledat"
