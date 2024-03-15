97b0cf691ebba1c104045fa1fb05513e53cf0911
Kristen Dybala <kdybala@gmail.com>
2024-03-14 17:48:18 -0700

    update to real sample data

--- R/sampledat.R
+++ R/sampledat.R
@@ -1,8 +1,23 @@
 #' Sample Water Tracker data for demonstrating package functions
-#' @format
-#' A tibble with 369 rows and 10 columns
+#'
+#' @description Sample hydrological data for wetland complexes in the Central
+#'   Valley. This data set includes data extracted from Point Blue'
+#'   sWaterTracker from two wetland complexes:
+#'
+#'   "SampleWetland1" includes 9 individual units, of which all are expected to
+#'   be "managed" wetlands
+#'
+#'   "SampleWetland2" contains 8 units, and all but unit 8 are expected to be
+#'   "managed".
+#'
+#'
+#' @format A tibble with 7803 rows and 13 columns
 #' \describe{
-#'    \item{Name}{Name of sample wetland management unit}
+#'    \item{WETLAND}{Name of sample wetland complex}
+#'    \item{MU}{Unique ID of sample wetland management unit}
+#'    \item{CLASS}{Predicted status as "managed" wetland or "other"}
+#'    \item{AREA_HA}{Polygon area in hectares}
+#'    \item{AREA_AC}{Polygon area in acres}
 #'    \item{Mosaic}{Unique ID for the mosaic of satellite images included}
 #'    \item{MosaicDateStart}{Earliest date of satellite imagery mosaic}
 #'    \item{MosaicDateEnd}{Latest date of satelliate imagery mosaic}
--- data-raw/sampledat.R
+++ data-raw/sampledat.R
@@ -1,17 +1,38 @@
 ## code to prepare `sampledat` dataset goes here
 
-dat = readr::read_csv('C:/Users/kdybala/OneDrive - Point Blue/Documents/A_Projects_inactive/DRWP/data/flood_curves/cropscape2021_rice.csv')
+# # wetland polygons
+# mu = sf::read_sf('C:/Users/kdybala/OneDrive - Point Blue/Documents/A_Projects/2163_WaterSmart/water_smart_GIS/derived_data/wetland_units_update_20240126.shp')
+#
+# wetland class data
+pred = readr::read_csv('C:/Users/kdybala/OneDrive - Point Blue/Documents/A_Projects/2163_WaterSmart/data/wetlands_predicted.csv') |>
+  select(MU, pred.class) |>
+  mutate(CLASS = recode(pred.class, seas = 'managed')) |>
+  select(-pred.class)
 
-sampledat = dat |> dplyr::select(Name = BasinName, Mosaic:Threshold) |>
-  dplyr::mutate(Name = dplyr::recode(Name,
-                                     American = 'unit1',
-                                     Butte = 'unit2',
-                                     Colusa = 'unit3',
-                                     Delta = 'unit4',
-                                     `San Joaquin` = 'unit5',
-                                     Suisun = 'unit6',
-                                     Sutter = 'unit7',
-                                     Tulare = 'unit8',
-                                     Yolo = 'unit9'))
+dat = readr::read_csv('C:/Users/kdybala/OneDrive - Point Blue/Documents/A_Projects/2163_WaterSmart/data/combined_units-wetland_vers-20240126_method-zonal.csv') |>
+  left_join(pred, by = 'MU')
+#dat = readr::read_csv('C:/Users/kdybala/OneDrive - Point Blue/Documents/A_Projects/2163_WaterSmart/data/combined_units-wetland_version-20240126_method-zonal.csv')
+#dat = readr::read_csv('C:/Users/kdybala/OneDrive - Point Blue/Documents/A_Projects_inactive/DRWP/data/flood_curves/cropscape2021_rice.csv')
+
+
+sampledat = bind_rows(
+  dat |>
+    dplyr::select(WETLAND, FIELD, MU, CLASS, AREA_HA, AREA_AC, Mosaic:dplyr::last_col()) |>
+    filter(grepl('GWMA', MU) & FIELD == 'N-22') |>
+    dplyr::mutate(WETLAND = 'SampleWetland1',
+                  MU = as.numeric(as.factor(MU)),
+                  MU = paste0('SampleWetland1_Unit', MU)),
+  dat |>
+    dplyr::select(WETLAND, FIELD, MU, CLASS, AREA_HA, AREA_AC, Mosaic:dplyr::last_col()) |>
+    filter(grepl('SAC', MU) & FIELD %in% c('T11', 'T13')) |>
+    dplyr::mutate(WETLAND = 'SampleWetland2',
+                  MU = as.numeric(as.factor(MU)),
+                  MU = paste0('SampleWetland2_Unit', MU))
+  ) |>
+  select(-FIELD, -EstimatedAreaWaterHa) |>
+  arrange(WETLAND, MU)
+
+# summary:
+sampledat |> select(WETLAND, MU, CLASS) |> distinct()
 
 usethis::use_data(sampledat, overwrite = TRUE)
